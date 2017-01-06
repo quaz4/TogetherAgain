b274a65405f987f359fd25c13873a0513c02d5bc
quaz4 <hmaswill@gmail.com>
2017-01-06 21:25:47 +0800

    Added Navigation activity to act as the center of the app, all navigation, once logged in, starts from this activity.

--- mobile/src/main/java/net/dust_bowl/togetheragain/MainActivity.java
+++ mobile/src/main/java/net/dust_bowl/togetheragain/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
